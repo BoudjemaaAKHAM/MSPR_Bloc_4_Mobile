87642d05973139f52a09b3b72d4a90876dcb50e7
JDS38 <94697000+JDS38@users.noreply.github.com>
2023-05-03 09:15:41 +0200

    New Version. Fix implementation

---
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/ProductActivity.java
@@ -0,0 +1,78 @@
+package com.google.ar.core.examples.java.helloar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductActivity extends AppCompatActivity {
+
+    Button BtnHome;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+        BtnHome = findViewById(R.id.Btn_Home);
+
+        ListView listProduct = findViewById(R.id.listProduct);
+
+        List<String> list = new ArrayList<>();
+        list.add("Produit 1");
+        list.add("Produit 2");
+        list.add("Produit 3");
+        list.add("Produit 4");
+
+        ArrayAdapter arrayAdapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,list);
+        listProduct.setAdapter(arrayAdapter);
+
+        listProduct.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                if (i==0){
+                    //product 1
+                    Launch_ARactivity(i+1);
+                } else if (i==1){
+                    //product 2
+                    Launch_ARactivity(i+1);
+                } else if (i==2){
+                    //product 3
+                    Launch_ARactivity(i+1);
+                } else if (i==3){
+                    //product 4
+                    Launch_ARactivity(i+1);
+                } else{
+
+                }
+            }
+        });
+
+        BtnHome.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+    void Launch_ARactivity(int position){
+        Intent intent = new Intent(ProductActivity.this, HelloArActivity.class);
+        Bundle b = new Bundle();
+        b.putInt("key", position); //PRODUCT ID
+        intent.putExtras(b); //Put your id to your next Intent
+        startActivity(intent);
+        finish();
+    }
+
+    void openMainActivity(){
+        Intent intent = new Intent (this,Menu.class);
+        startActivity(intent);
+    }
+}
