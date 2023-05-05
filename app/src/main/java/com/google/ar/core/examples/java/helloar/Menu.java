a590379f20b2a00d725192ab35774c2028d29a21
BoudjemaaAKHAM <akhamboudjemaa@gmail.com>
2023-05-05 21:06:08 +0200

    get product from api is working now

--- app/src/main/java/com/google/ar/core/examples/java/helloar/Menu.java
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/Menu.java
@@ -19,7 +19,14 @@ import android.widget.Toast;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.Socket;
@@ -29,7 +36,11 @@ import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
+import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManager;
@@ -43,10 +54,9 @@ public class Menu extends AppCompatActivity {
     public Boolean valid;
 
     public static final String SHARED_PREFS = "sharedPrefs";
-    public static final String TEXT="text";
+    public static final String TEXT = "text";
 
-    public static final String STATET= "state";
-
+    public static final String STATET = "state";
 
 
     private String text;
@@ -76,12 +86,10 @@ public class Menu extends AppCompatActivity {
         TextView textView = (TextView) findViewById(R.id.Logged);
 
         loadData();
-        state=statet;
+        state = statet;
         textView.setText("User Connected : " + text);
         updateViews(text);
         BtnProduct.setEnabled(state);
-
-
 
 
         BtnLogOut.setOnClickListener(v ->
@@ -92,7 +100,7 @@ public class Menu extends AppCompatActivity {
 
         //BtnScan.setOnClickListener(v ->
         //{
-            //LogIn("Test");
+        //LogIn("Test");
 
         //});
         BtnScan.setOnClickListener(new View.OnClickListener() {
@@ -134,22 +142,16 @@ public class Menu extends AppCompatActivity {
                 Log.d(TAG, "Contenu du code QR : " + result.getContents());
                 Toast.makeText(this, "Contenu du code QR : " + result.getContents(), Toast.LENGTH_SHORT).show();
 
-
                 startApiThread(result.getContents());
 
-
-
-
-
-                // Utilisez le contenu du QR code comme vous le souhaitez dans votre application
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
     }
 
-    void openProductActivity(){
-        Intent intent = new Intent (this,ProductActivity.class);
+    void openProductActivity() {
+        Intent intent = new Intent(this, ProductActivity.class);
         startActivity(intent);
     }
 
@@ -160,100 +162,101 @@ public class Menu extends AppCompatActivity {
             state = true;
             BtnProduct.setEnabled(state);
         }
-        if(user == "No One"){
+        if (user == "No One") {
             state = false;
             BtnProduct.setEnabled(state);
         }
         Log.d(TAG, "Update do: " + user + " " + state);
     }
 
-    private void LogOut(String user){
+    private void LogOut(String user) {
         TextView textView = (TextView) findViewById(R.id.Logged);
-        textView.setText("User Connected : "+user);
-        if (user == "No One"){
-            state=false;
+        textView.setText("User Connected : " + user);
+        if (user == "No One") {
+            state = false;
             BtnProduct.setEnabled(state);
-            saveData(user,state);
+            saveData(user, state);
         }
         AlertDialog.Builder builder = new AlertDialog.Builder(Menu.this);
         builder.setTitle("Disconnected");
 
         builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
             @Override
-            public void onClick(DialogInterface dialogInterface, int i)
-            {
+            public void onClick(DialogInterface dialogInterface, int i) {
                 dialogInterface.dismiss();
             }
         }).show();
     }
 
 
-    private void saveData(String log,Boolean logg){
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
+    private void saveData(String log, Boolean logg) {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
 
 
-
-        editor.putString(TEXT,log);
-        editor.putBoolean(STATET,logg);
+        editor.putString(TEXT, log);
+        editor.putBoolean(STATET, logg);
 
         editor.apply();
         if (log == "No One")
-            Toast.makeText(this,"Seller DisLogged",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Seller DisLogged", Toast.LENGTH_SHORT).show();
         if (log == "Seller")
-            Toast.makeText(this,"Seller Logged",Toast.LENGTH_SHORT).show();
-        Log.d(TAG, "Contenu saved: " + log + " " + logg);
+            Toast.makeText(this, "Seller Logged", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "Content saved: " + log + " " + logg);
 
-    };
+    }
 
-    private void loadData(){
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS,MODE_PRIVATE);
-        text = sharedPreferences.getString(TEXT,"No One");
-        statet = sharedPreferences.getBoolean(STATET,false);
+    private void loadData() {
+        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+        text = sharedPreferences.getString(TEXT, "No One");
+        statet = sharedPreferences.getBoolean(STATET, false);
         Log.d(TAG, "Contenu read: " + text + " " + statet);
-    };
+    }
 
     private void askAPI(String token) {
-        String urlString = "http://13.53.243.55:444/api/v1/products";
-        //String urlString = "https://www.google.com";
-
+        String urlString = "https://615f5fb4f7254d0017068109.mockapi.io/api/v1/products";
+        // String urlString = "https://16.16.4.35:444/api/v1/products";
         int responseCode = 0;
-        String concat = null;
-        URL url = null;
-        concat = urlString + " " + token;
         try {
-            url = new URL(urlString);
-            Log.d(TAG, "REQUEST:test1 ");
+            // désactive la vérification du certificat SSL car auto-signé
+            // disableSSLCertificateChecking();
+            URL url = new URL(urlString);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            Log.d(TAG, "REQUEST:test2 ");
             connection.setRequestMethod("GET");
-            Log.d(TAG, "REQUEST:test3 ");
             connection.setConnectTimeout(5000); // Timeout de connexion de 5 secondes
-            Log.d(TAG, "REQUEST:test4 ");
             connection.setRequestProperty("Authorization", "Bearer " + token);
-            Log.d(TAG, "REQUEST:test5 ");
-            //trustSelfSignedSSL();
-            Log.d(TAG, "REQUEST:test5.5 ");
             connection.connect();
-            Log.d(TAG, "REQUEST:test6 "+connection.toString());
-            Log.d(TAG, "CONNECTION URL: " + connection.getURL());
-            Log.d(TAG, "REQUEST METHOD: " + connection.getRequestMethod());
-            Log.d(TAG, "REQUEST PROPERTIES: " + connection.getRequestProperty("Authorization"));
             responseCode = connection.getResponseCode();
 
             if (responseCode == HttpURLConnection.HTTP_OK) {
                 Log.d(TAG, "REQUEST: L'URL est valide et atteignable.");
                 valid = true;
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line = "";
+                StringBuilder data = new StringBuilder();
+                while (line != null) {
+                    line = bufferedReader.readLine();
+                    data.append(line);
+                }
+                List<Object> products = convertResponseToProducts(String.valueOf(data));
+
+                // products contien la liste des produits
+                // voici comment itérer sur cette liste
+                for (Object product : products) {
+                    System.out.println(product.toString());
+                    Log.d(TAG, "PRODUCT: " + product.toString());
+                }
 
             } else {
-                Log.d(TAG, "REQUEST: L'URL est valide mais n'est pas atteignable (concat : " + concat + " code de réponse HTTP : " + responseCode + ").");
+                Log.d(TAG, "REQUEST: L'URL est valide mais n'est pas atteignable code de réponse HTTP : " + responseCode + ").");
                 valid = false;
             }
         } catch (MalformedURLException e) {
-            Log.d(TAG, "REQUEST: L'URL est invalide.(concat : " + concat + " code de réponse HTTP : " + responseCode + ").");
+            Log.d(TAG, "REQUEST: L'URL est invalide. code de réponse HTTP : " + responseCode + ").");
             valid = false;
         } catch (IOException e) {
-            Log.d(TAG, "REQUEST: L'URL est invalide ou n'est pas atteignable. (concat : " + concat + " code de réponse HTTP : " + responseCode + ").");
+            Log.d(TAG, "REQUEST: L'URL est invalide ou n'est pas atteignable. code de réponse HTTP : " + responseCode + ").");
             valid = false;
         }
         //catch (NoSuchAlgorithmException e) {
@@ -267,6 +270,53 @@ public class Menu extends AppCompatActivity {
         //}
     }
 
+    private void disableSSLCertificateChecking() {
+        // Create a trust manager that does not validate certificate chains
+
+        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        }};
+
+        // Install the all-trusting trust manager
+        try {
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = (hostname, session) -> true;
+
+        // Install the all-trusting host verifier
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+    }
+
+
+    private List<Object> convertResponseToProducts(String response) {
+        List<Object> products = new ArrayList<Object>();
+        try {
+            JSONArray jsonArray = new JSONArray(response);
+            for (int i = 0; i < jsonArray.length(); i++) {
+                JSONObject product = jsonArray.getJSONObject(i);
+                products.add(product);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return products;
+    }
+
     private void startApiThread(final String token) {
         Thread apiThread = new Thread(new Runnable() {
             @Override
@@ -276,10 +326,10 @@ public class Menu extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        if (valid == true){
-                            username="Seller";
-                            state=true;
-                            saveData(username,state);
+                        if (valid == true) {
+                            username = "Seller";
+                            state = true;
+                            saveData(username, state);
                             updateViews(username);
 
                         } else {
@@ -288,8 +338,7 @@ public class Menu extends AppCompatActivity {
 
                             builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
                                 @Override
-                                public void onClick(DialogInterface dialogInterface, int i)
-                                {
+                                public void onClick(DialogInterface dialogInterface, int i) {
                                     dialogInterface.dismiss();
                                 }
                             }).show();
@@ -300,24 +349,5 @@ public class Menu extends AppCompatActivity {
         });
         apiThread.start();
     }
-
-    private static void trustSelfSignedSSL() throws NoSuchAlgorithmException, KeyManagementException {
-        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
-            public X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-
-            public void checkClientTrusted(X509Certificate[] certs, String authType) {
-            }
-
-            public void checkServerTrusted(X509Certificate[] certs, String authType) {
-            }
-        } };
-
-        SSLContext sc = SSLContext.getInstance("SSL");
-        sc.init(null, trustAllCerts, new SecureRandom());
-        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-    }
-
 
 }
